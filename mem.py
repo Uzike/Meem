664a232bdaa50fc2009cf98ff6741832d54e7dc3
Uzike <133394137+Uzike@users.noreply.github.com>
2023-06-07 20:19:04 +0400

    2nd mem.py
    pain

--- mem.py
+++ mem.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands 
+from my_token import my_token
+import os
+from random import randint, choice 
+import requests
+from fayl import get_redd_mem
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -10,10 +15,23 @@ bot = commands.Bot(command_prefic='&', intents=intents)
 async def on_ready():
     print("All good.")
 
-@bot.command()
+@bot.command('mem')
 async def mem(ctx):
     with open('images/mem1.jpg', 'rb') as f:
         picture = discord.File(f)
     await ctx.sent(file=picture)
+    
+@bot.command('random_mem')
+asynd def random_mem(ctx):
+    mems = os.listdir('images')
+    meem = choice(mems)
+    with open(f'images/{meem}", rb) as f:
+              picture = discord.File(f)
+    await ctx.send(file=picture)           
 
+@bot.command('redd')
+async def redd(ctx):
+    image_url = get_redd_mem()
+    await ctx.send(image_url)
+              
 bot.run("...")
